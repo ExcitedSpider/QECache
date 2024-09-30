c481a4810afd842364b55ff81beec99f0f459823
Chew F <chew.y.feng@outlook.com>
2024-09-30 20:24:46 +1000

    feat: byteview class

---
+++ byteview.go
@@ -0,0 +1,22 @@
+package qecache
+
+// A structure represent the cached value
+// use byte for its universality
+type ByteView struct {
+	value []byte
+}
+
+func (v ByteView) Len() int {
+	return len(v.value)
+}
+
+// The slice return a copy of value
+func (v ByteView) ByteSlice() []byte {
+	clone := make([]byte, v.Len())
+	copy(clone, v.value)
+	return clone
+}
+
+func (v ByteView) String() string {
+	return string(v.value)
+}
