f2798b2d0f206104486a7f65aede3b91a8364b3b
Chew F <chew.y.feng@outlook.com>
2024-10-03 20:32:31 +1000

    feat: test multi nodes example

---
+++ examples/multi-nodes/go.mod
@@ -0,0 +1,5 @@
+module example/multi-nodes
+
+go 1.23.1
+require QECache v0.0.0
+replace QECache => ../../.
---
+++ examples/multi-nodes/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	qecache "QECache"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func createController() *qecache.Controller {
+	return qecache.NewController("scores", 2<<10, qecache.FetcherFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+}
+
+func startCacheServer(addr string, addrs []string, gee *qecache.Controller) {
+	peers := qecache.NewHTTPServer(qecache.HTTPServerConfig{
+		SelfIP: addr,
+	})
+	peers.SetPeers(addrs...)
+	gee.RegisterPeers(peers)
+	log.Println("qecache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
+}
+
+func startAPIServer(apiAddr string, gee *qecache.Controller) {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+			view, err := gee.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(view.ByteSlice())
+
+		}))
+	log.Println("fontend server is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+
+}
+
+func main() {
+	var port int
+	var api bool
+	flag.IntVar(&port, "port", 8001, "Geecache server port")
+	flag.BoolVar(&api, "api", false, "Start a api server?")
+	flag.Parse()
+
+	apiAddr := "http://localhost:9999"
+	addrMap := map[int]string{
+		8001: "http://localhost:8001",
+		8002: "http://localhost:8002",
+		8003: "http://localhost:8003",
+	}
+
+	var addrs []string
+	for _, v := range addrMap {
+		addrs = append(addrs, v)
+	}
+
+	gee := createController()
+	if api {
+		go startAPIServer(apiAddr, gee)
+	}
+	startCacheServer(addrMap[port], []string(addrs), gee)
+}
